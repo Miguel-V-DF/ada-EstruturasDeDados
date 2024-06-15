e675065d626baac8b78f9695a737f5de9291ad0c
Miguel V_DF <miguelvittadev@gmail.com>
2024-06-14 22:26:14 -0300

    added the prepend function

--- src/main/java/dataStructure/LinkedList.java
+++ src/main/java/dataStructure/LinkedList.java
@@ -98,6 +98,18 @@ public class LinkedList {
 
     }
 
+    public void prepend(String data) {
+        Node newNode = new Node(data);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
 
 
 
@@ -106,8 +118,9 @@ public class LinkedList {
         LinkedList list = new LinkedList("elemento 1");
         list.append("elemento 2");
         list.append("elemento 3");
+        list.prepend("elemento 0");
 
-        System.out.println(list.removeLast().data);
+//        System.out.println(list.removeLast().data);
         list.print();
 
 
