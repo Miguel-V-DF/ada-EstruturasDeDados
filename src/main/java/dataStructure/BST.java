c6a949c565cf0cc679853671940b57b3d84f8b76
Miguel V_DF <miguelvittadev@gmail.com>
2024-06-15 21:17:19 -0300

    different test of BST

--- src/main/java/dataStructure/BST.java
+++ src/main/java/dataStructure/BST.java
@@ -48,9 +48,13 @@ public class BST {
 
     public static void main(String[] args) {
         BST tree = new BST();
-        tree.insert(2);
-        tree.insert(1);
-        tree.insert(3);
+        tree.insert(37);
+        tree.insert(66);
+        tree.insert(42);
+        tree.insert(11);
+        tree.insert(72);
+        tree.insert(8);
+        tree.insert(17);
 
         tree.inOrder();
     }
